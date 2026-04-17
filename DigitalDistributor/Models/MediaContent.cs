00133c779afd382e2f73806553acc026213b30c6
envelopehub <kino.lover.ua@gmail.com>
2026-04-17 13:25:18 +0300

    Day 7: Implemented MVVM basic structure, INotifyPropertyChanged, ObservableCollection, and ItemsControl DataBinding

---
+++ DigitalDistributor/Models/MediaContent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DigitalDistributor.Models
+{
+    public enum ContentType { Game, Movie, Software }
+
+    // Реалізуємо INotifyPropertyChanged для оновлення UI в реальному часі
+    public class MediaContent : INotifyPropertyChanged
+    {
+        private Guid _id = Guid.NewGuid();
+        private string _title = string.Empty;
+        private string _genre = string.Empty;
+        private decimal _price;
+        private ContentType _type;
+
+        public Guid Id
+        {
+            get => _id;
+            set { _id = value; OnPropertyChanged(); }
+        }
+
+        public string Title
+        {
+            get => _title;
+            set { _title = value; OnPropertyChanged(); }
+        }
+
+        public string Genre
+        {
+            get => _genre;
+            set { _genre = value; OnPropertyChanged(); }
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            set { _price = value; OnPropertyChanged(); }
+        }
+
+        public ContentType Type
+        {
+            get => _type;
+            set { _type = value; OnPropertyChanged(); }
+        }
+
+        // Подія, яка сповіщає XAML про зміну значення
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ DigitalDistributor/ViewModels/CatalogViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using DigitalDistributor.Models;
+
+namespace DigitalDistributor.ViewModels
+{
+    public class CatalogViewModel
+    {
+        // Колекція, яка автоматично оновлює UI при додаванні/видаленні елементів
+        public ObservableCollection<MediaContent> ContentList { get; set; }
+
+        public CatalogViewModel()
+        {
+            // Поки що заповнимо тестовими даними (пізніше будемо читати з JSON)
+            ContentList = new ObservableCollection<MediaContent>
+            {
+                new MediaContent { Title = "The Witcher 3: Wild Hunt", Genre = "RPG", Price = 599.99m, Type = ContentType.Game },
+                new MediaContent { Title = "Cyberpunk 2077", Genre = "Action/RPG", Price = 899.00m, Type = ContentType.Game },
+                new MediaContent { Title = "Adobe Photoshop", Genre = "Графіка", Price = 1200.50m, Type = ContentType.Software },
+                new MediaContent { Title = "Interstellar", Genre = "Наукова фантастика", Price = 150.00m, Type = ContentType.Movie }
+            };
+        }
+    }
+}
